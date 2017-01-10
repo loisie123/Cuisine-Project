705cf47583e8276fc85df92b8e76113c1f8b43ef
loisie123 <lois.vanvliet@student.uva.nl>
2017-01-10 12:33:31 +0100

    Initial Commit

---
+++ Cuisine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cuisine
+//
+//  Created by Lois van Vliet on 10-01-17.
+//  Copyright © 2017 Lois van Vliet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
